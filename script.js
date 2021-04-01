23b4fd1699d76ce63e868f043b3df7d4f605a723
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-04-01 09:49:20 -0300

    seletor em cada lista de itens

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 function PratoEscolhido() {
 
-    const escolhido = document.querySelector("li");
+    const escolhido = document.querySelector(".prato");
     escolhido.classList.add("green-border");
 
     const icone = document.querySelector("ion-icon");
     icone.classList.remove("hidden");
-
 }
 
+function BebidaEscolhida() {
+
+    const escolhido = document.querySelector(".Bebida");
+    escolhido.classList.add("green-border");
+
+    const icone = document.querySelector("ion-icon");
+    icone.classList.remove("hidden");
+}
+
+function SobremesaEscolhida() {
+
+    const escolhido = document.querySelector(".Sobremesa");
+    escolhido.classList.add("green-border");
+
+    const icone = document.querySelector("ion-icon");
+    icone.classList.remove("hidden");
+}
+
+// function FecharPedido() {
+//     const FinalPrice = document.querySelector("price");
+// }
